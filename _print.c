bb270b24f1cb86bf263df1c7f323800c377ef982
Glo-ma <ademolaglory2003@gmail.com>
2020-10-27 19:57:59 +0000

    Print functions

--- _print.c
+++ _print.c
@@ -3,6 +3,26 @@
 #include <unistd.h>
 
 /**
+ *_strlen- Gets the length of a string
+ *@s: string
+ *
+ *return:Length of a string
+ */
+
+int _strlen(char *s)
+{
+
+        int length = 0;
+
+        while (*s)
+        {
+                s++;
+                length++;
+        }
+        return (length);
+}
+
+/**
  *_printf- Prints anything to output
  *@format:Parameter 1
  *@...:Variable number of arguements
@@ -11,41 +31,54 @@
 
 int _printf(const char *format, ...)
 {
-int i = 0;
-int length = 0;
-char char_arg;
-char *string_arg;
-va_list arguments;
+        int i = 0;
+        int length = 0;
+        char char_arg;
+        char *string_arg;
+        va_list arguments;
+        int j;
 
-va_start(arguments, format);
-while (*(format + i))
-{
-if ((*(format + i)) != '%')
-{
-write(STDOUT_FILENO, (format + i), 1);
-length++;
-}
-else
-{
-i += 1;
-switch (*(format + i))
-{
-case 'c':
-char_arg = va_arg(arguments, int);
+        va_start(arguments, format);
+        while (*(format + i))
+        {
+                if ((*(format + i)) != '%')
+                {
+                        if (write(1, format + i, 1) == -1)
+                                return (-1);
+                        length++;
+                }
+                else
+                {
+                        i += 1;
+                        switch (*(format + i))
+                        {
+			case 'c':
+				char_arg = va_arg(arguments, int);
 
-write(STDOUT_FILENO, &char_arg, 1);
-length++;
-break;
-case 's':
-string_arg = va_arg(arguments, char *);
+				j = write(1, &char_arg, 1);
+				if (j == -1)
+					return (-1);
+				length++;
+				break;
+			case 's':
+				string_arg = va_arg(arguments, char *);
 
-write(STDOUT_FILENO, string_arg, (sizeof(string_arg)) - 1);
-length++;
-break;
-}
+				j = write(1, string_arg, _strlen(string_arg));
+				if (j == -1)
+					return (-1);
+				length += j;
+				break;
+			case '%':
+				j = write(1, &("%"), 1);
+				if (j == -1)
+					return (-1);
+				length++;
+				break;
 
-}
-i++;
-}
-return (length);
+                        }
+
+                }
+                i++;
+        }
+        return (length);
 }
