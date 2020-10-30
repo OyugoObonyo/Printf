fdca6cfe3f8fb4d2a75defa4df3f8a12962780b1
OyugoObonyo <nyikal1999@gmail.com>
2020-10-30 19:17:50 +0000

    Header File

--- holberton.h
+++ holberton.h
@@ -1,7 +1,16 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+int _putchar(char c);
+int print_str(va_list arg);
+int print_number(va_list arg);
+int handle_switch(char format, va_list arguments);
 int _printf(const char *format, ...);
 int _strlen(char *s);
+int print_char(va_list arg);
 
 #endif
