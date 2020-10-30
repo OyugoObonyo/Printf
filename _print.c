4142242317f7ee5aeb4d0256977e170cdd137200
OyugoObonyo <nyikal1999@gmail.com>
2020-10-30 19:59:02 +0000

    _printf function

--- _print.c
+++ _print.c
@@ -3,26 +3,6 @@
 #include <unistd.h>
 
 /**
- *_strlen- Gets the length of a string
- *@s: string
- *
- *return:Length of a string
- */
-
-int _strlen(char *s)
-{
-
-        int length = 0;
-
-        while (*s)
-        {
-                s++;
-                length++;
-        }
-        return (length);
-}
-
-/**
  *_printf- Prints anything to output
  *@format:Parameter 1
  *@...:Variable number of arguements
@@ -31,54 +11,42 @@ int _strlen(char *s)
 
 int _printf(const char *format, ...)
 {
-        int i = 0;
-        int length = 0;
-        char char_arg;
-        char *string_arg;
-        va_list arguments;
-        int j;
+int length = 0, checkflag = 0, i = 0;
+va_list arguments;
+char flag[] = {'c', 's', 'd', 'i', '%'};
 
-        va_start(arguments, format);
-        while (*(format + i))
-        {
-                if ((*(format + i)) != '%')
-                {
-                        if (write(1, format + i, 1) == -1)
-                                return (-1);
-                        length++;
-                }
-                else
-                {
-                        i += 1;
-                        switch (*(format + i))
-                        {
-			case 'c':
-				char_arg = va_arg(arguments, int);
-
-				j = write(1, &char_arg, 1);
-				if (j == -1)
-					return (-1);
-				length++;
-				break;
-			case 's':
-				string_arg = va_arg(arguments, char *);
-
-				j = write(1, string_arg, _strlen(string_arg));
-				if (j == -1)
-					return (-1);
-				length += j;
-				break;
-			case '%':
-				j = write(1, &("%"), 1);
-				if (j == -1)
-					return (-1);
-				length++;
-				break;
-
-                        }
-
-                }
-                i++;
-        }
-        return (length);
+va_start(arguments, format);
+if (format == NULL)
+return (-1);
+while (*format)
+{
+if (*format != '%')
+{
+if (write(1, format, 1) == -1)
+return (-1);
+length++;
+}
+else
+{
+format++;
+if (!*format)
+return (-1);
+while (flag[i] != '\0')
+{
+if (*format == flag[i++])
+checkflag = 1;
+}
+if (checkflag == 1)
+length += handle_switch(*format, arguments);
+else
+{
+_putchar('%');
+_putchar(*format);
+length++;
+length++;
+}
+}
+format++;
+}
+return (length);
 }
